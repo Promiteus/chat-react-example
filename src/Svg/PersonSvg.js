de139a3915868eac7248bf285ed2873fcd6e4098
Roman Matveev <r.matveev@socrat.marketing>
2021-12-17 13:08:03 +1000

    17.12.2021 -- Переработка формы регистрации и входа.Добавление векторных иконок

---
+++ src/Svg/PersonSvg.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Person({onClick, w, h, color}) {
+    return (
+      <div onClick={onClick}>
+        <svg width={w || 28} height={h || 28} fill={color || 'blue'}  viewBox="0 0 16 16">
+            <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
+        </svg>
+      </div>
+    )
+}
+
