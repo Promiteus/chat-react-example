9e742ae34a16f6cbc67c4f1a09fac7d9dbfcb48d
Roman Matveev <r.matveev@socrat.marketing>
2021-12-31 13:58:23 +1000

    31.12.2021 -- Подключение к MessageView.jsx и UserList.jsx методов доступа к данным переписки. Часть 2. Запонение сообщениями панели чата.

--- src/Chat/MessageView/MessageItem/MessageItem.jsx
+++ src/Chat/MessageView/MessageItem/MessageItem.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import MessageItemElement from '../MessageItemElement/MessageItemElement';
 import './MessageItem.css';
 
-const MessageItem = ({msg}) => (
+const MessageItem = ({data}) => (
   <div className="MessageItem p-1 mt-2 d-flex">
-      <MessageItemElement>{msg}</MessageItemElement>
+      <MessageItemElement>{data.message}</MessageItemElement>
   </div>
 );
 
--- src/Chat/MessageView/MessageView.jsx
+++ src/Chat/MessageView/MessageView.jsx
@@ -3,37 +3,53 @@ import MessageItem from './MessageItem/MessageItem';
 import './MessageView.css'
 import {useSelector} from "react-redux";
 import {selectChat} from "../../Stores/slices/ChatSlice";
+import {v4 as uuidv4} from 'uuid';
 
-function MessageView({stomp}) {
+function MessageView({stomp, userId}) {
   const [messageList, setMessageList] = useState([]);
   const {status, response, loading} = useSelector(selectChat);
-
 
   useEffect(() => {
     if (stomp) {
       stomp.onMessageReceived = (data) => {
         let body = JSON.parse(data.body);
 
-        if ((body) && (body.content)) {
-          setMessageList(prev => [...prev, body.content]);
+        if ((body) && (body?.content)) {
+          setMessageList(prev => [...prev, {
+            id: uuidv4().replace('-', ''),
+            userId: "201",
+            fromUserId: userId,
+            message: body.content,
+          }]);
         }
       };
     }
   });
+
+  /*
+   * Так выгдядят сохраненные сообщения чата
+   data:
+   [{"id":"61a0343931b2e54902c1b0b4","userId":"200","fromUserId":"203","message":"Сообщение от Konstantin номер 8.","timestamp":"2021-11-26 01:11:21"},
+    {"id":"61a0343931b2e54902c1b0b5","userId":"200","fromUserId":"203","message":"Сообщение от Konstantin номер 9.","timestamp":"2021-11-26 01:11:21"},]
+  * */
 
   //Реагировать на смену статуса при запросе последних сообщений из чата
   useEffect(() => {
      console.log("MessageView status: "+status);
      if (+status === 200) {
          console.log("MessageView response: "+JSON.stringify(response?.data));
-         
+         //Предочистка сиска сообщений перед переключением между пользователями
+         setMessageList([]);
+         //Обновить список сообщений для выбранного пользователя
+         response?.data?.forEach(elem => {
+              setMessageList(prevState => [...prevState, elem ]);
+         });
      }
-
-  }, [status])
+  }, [status]);
   
   return (
     <div className="chatView d-flex flex-column" >
-      {messageList.map((data, index) => (<MessageItem key={index} msg={data}/>))}    
+      {messageList.map((element) => (<MessageItem key={element?.id} data={element}/>))}
     </div>
   );
 };
