ea397b1130d2e1ec9e8000894addfd5982f7ebf6
Roman Matveev <r.matveev@socrat.marketing>
2022-01-16 20:08:28 +1000

    16.01.2022 -- Корректировка стиля списка чатов.

--- src/Chat/Users/UserList/UserProfile/UserProfile.jsx
+++ src/Chat/Users/UserList/UserProfile/UserProfile.jsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import RightChevron from "../../../../Svg/RightChevron";
 import './UserProfile.css';
-import {Avatar, Paper, Typography} from "@mui/material";
+import {Avatar, Box, Divider, Paper, Typography} from "@mui/material";
 import {dateDiffYears} from "../../../../Componetns/DateHandlers";
 import {stringAvatar} from "../../../../Componetns/GraphicHandler";
 import {BASE_DATA_URL} from "../../../../Stores/api/Common/ApiCommon";
@@ -23,26 +23,29 @@ export default function Userprofile({user, selected, onClick}) {
     }
 
     return (
-        <Paper elevation={2} sx={{'&:hover': { backgroundColor: 'primary.main', opacity: [0.9, 0.8, 0.7],}}}
-             className="d-flex flex-row "
-             style={styles.redBg}
-             onClick={() => onClick({user})}>
-          <div className="border-dark d-flex p-1">
-              {user?.thumbUrl ?
-                  <Avatar alt={user?.firstName} src={`${BASE_DATA_URL}${user?.thumbUrl}`}/> :
-                  <Avatar {...stringAvatar(`${user?.firstName} ${user?.lastName}`)} />
-              }
-          </div>
-          <div className="d-flex flex-row justify-content-between flex-grow-1">
-              <div className="d-flex flex-column">
-                  <Typography variant={"subtitle1"}><b>{user?.firstName} {user?.lastName}</b></Typography>
-                  <Typography variant={"body2"}>Возраст: {dateDiffYears(user?.birthDate, new Date().toDateString())}</Typography>
-              </div>
-              <div className="mt-2">
-                  <RightChevron/>
-              </div>
-          </div>
-        </Paper>
+        <div>
+            <Box sx={{'&:hover': { backgroundColor: 'primary.main', opacity: [0.9, 0.8, 0.7],}}}
+                 className="d-flex flex-row "
+                 style={styles.redBg}
+                 onClick={() => onClick({user})}>
+                <div className="border-dark d-flex p-1">
+                    {user?.thumbUrl ?
+                        <Avatar alt={user?.firstName} src={`${BASE_DATA_URL}${user?.thumbUrl}`}/> :
+                        <Avatar {...stringAvatar(`${user?.firstName} ${user?.lastName}`)} />
+                    }
+                </div>
+                <div className="d-flex flex-row justify-content-between flex-grow-1">
+                    <div className="d-flex flex-column">
+                        <Typography variant={"subtitle1"}><b>{user?.firstName} {user?.lastName}</b></Typography>
+                        <Typography variant={"body2"}>Возраст: {dateDiffYears(user?.birthDate, new Date().toDateString())}</Typography>
+                    </div>
+                    <div className="mt-2">
+                        <RightChevron/>
+                    </div>
+                </div>
+            </Box>
+            <Divider/>
+        </div>
     );
 
 
