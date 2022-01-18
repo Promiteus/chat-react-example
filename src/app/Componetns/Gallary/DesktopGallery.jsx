65552c1fd2c63fbc3adfe8abe3162c9445d6a9c9
Roman Matveev <r.matveev@socrat.marketing>
2022-01-18 22:22:46 +1000

    18.01.2022 -- Создание раздела с описанием профиля пользователя. Часть 3. Создание компонента DesktopGallery.jsx

---
+++ src/app/Componetns/Gallary/DesktopGallery.jsx
@@ -0,0 +1,41 @@
+import React, {useState} from "react";
+import {BASE_DATA_URL} from "../../Stores/api/Common/ApiCommon";
+import {Divider} from "@mui/material";
+//import ImgsViewer from "react-images-viewer/src/ImgsViewer";
+
+
+/**
+ * ProfileDetail profile:
+ * {
+ * "id":"200",
+ * ...
+ * "imgUrls":[
+ * "/api/resource?user_id=200&file_id=ford_mustang_ford_avtomobil_226678_1280x1024.jpg",
+ * "/api/resource?user_id=200&file_id=Снимок экрана от 2021-10-12 14-22-20.png",
+ * "/api/resource?user_id=200&file_id=Снимок экрана от 2021-10-07 11-47-49.png"
+ * ],"thumbUrl":"/api/resource/thumb?user_id=200"
+ * }
+ *
+ * */
+
+const DesktopGallery = ({profile}) => {
+    const [isOpen, setOpen] = useState();
+
+    function getFullUrls(profile) {
+        return (profile?.imgUrls?.length > 0) ? profile?.imgUrls.map(elem => ({src: `${BASE_DATA_URL}${elem}`})) : [{src: ''}];
+    }
+
+    return(
+        <>
+            <div className="d-flex flex-row justify-content-center align-content-center">
+                {/*<ImgsViewer
+                    imgs={getFullUrls(profile)}
+                    isOpen={isOpen}/>*/}
+            </div>
+            <Divider/>
+        </>
+
+    )
+}
+
+export default DesktopGallery;
--- src/app/ProfileDetails/ProfileDetail.jsx
+++ src/app/ProfileDetails/ProfileDetail.jsx
@@ -2,6 +2,7 @@ import React, {useEffect} from "react";
 import StandartHeader from "../Componetns/Header/StandartHeader";
 import {useDispatch} from "react-redux";
 import {setPageIndex} from "../Stores/slices/CommonSlice";
+import DesktopGallery from "../Componetns/Gallary/DesktopGallery";
 
 const ProfileDetail = ({profile}) => {
     const commonDispatch = useDispatch();
@@ -17,6 +18,7 @@ const ProfileDetail = ({profile}) => {
     return (
         <div className="d-flex flex-column h-100">
             <StandartHeader onClick={onBack}/>
+            <DesktopGallery profile={profile}/>
             <div style={{overflowY: 'scroll'}} className="d-flex flex-grow-3 h-100">
 
             </div>
