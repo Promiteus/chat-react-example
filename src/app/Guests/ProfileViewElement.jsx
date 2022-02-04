a3b20828eea7b5c430977f8bf882a787c3141365
Roman Matveev <r.matveev@socrat.marketing>
2022-02-04 22:32:20 +1000

    04.02.2022 -- Постановка пустых фото.

--- src/app/Guests/ProfileViewElement.jsx
+++ src/app/Guests/ProfileViewElement.jsx
@@ -7,6 +7,7 @@ import {BASE_DATA_URL} from "../Stores/api/Common/ApiCommon";
 import {useDispatch} from "react-redux";
 import {defineSelectedUser, setPageIndex} from "../Stores/slices/CommonSlice";
 import {Chat} from "@mui/icons-material";
+import {NO_PHOTO_JPG, NO_PHOTO_PNG} from "../../assets";
 
 
 const ProfileViewElement = ({profile}) => {
@@ -15,17 +16,25 @@ const ProfileViewElement = ({profile}) => {
     return(
         <>
           <ImageListItem sx={{boxShadow: 'gray'}} className="m-1">
-              <img
-                  className="photo-card"
-                  src={`${BASE_DATA_URL}${profile?.thumbUrl?.src}`}
-                  srcSet={`${BASE_DATA_URL}${profile?.thumbUrl?.src}`}
-                  alt={`${profile?.firstName} ${profile?.lastName}`}
-                  loading="lazy"
-                  onClick={() => {
-                      dispatch(setPageIndex(1));
-                      dispatch(defineSelectedUser(profile));
-                  }}
-              />
+              {(profile?.thumbUrl?.src !== '') ?
+                  <img
+                      className="photo-card"
+                      src={`${BASE_DATA_URL}${profile?.thumbUrl?.src}`}
+                      srcSet={`${BASE_DATA_URL}${profile?.thumbUrl?.src}`}
+                      alt={`${profile?.firstName} ${profile?.lastName}`}
+                      loading="lazy"
+                      onClick={() => {
+                          dispatch(setPageIndex(1));
+                          dispatch(defineSelectedUser(profile));
+                      }}
+                  /> :
+                  <img
+                      className="photo-card"
+                      alt={`${profile?.firstName} ${profile?.lastName}`}
+                      src={NO_PHOTO_PNG}
+                  />
+              }
+
               <ImageListItemBar
                   title={`${profile?.firstName} ${profile?.lastName}`}
                   subtitle={""}
--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,9 @@
 import IMG_LOGO from './logo.png';
+import NO_PHOTO_JPG from './no_photo.jpg';
+import NO_PHOTO_PNG from './no_photo.png';
 
 export {
     IMG_LOGO,
+    NO_PHOTO_PNG,
+    NO_PHOTO_JPG,
 }
