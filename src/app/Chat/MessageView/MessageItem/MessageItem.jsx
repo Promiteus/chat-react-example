a0adfdbf53d5211677bd9537f6cb1c6ef7ce35f0
Roman Matveev <r.matveev@socrat.marketing>
2022-06-09 10:55:57 +1000

    08.06.2022 -- Установлен отсутп справа от имени пользователя до даты, чтобы эти два поля не сливались на маленьких экранах.

--- src/app/Chat/MessageView/MessageItem/MessageItem.jsx
+++ src/app/Chat/MessageView/MessageItem/MessageItem.jsx
@@ -11,9 +11,9 @@ function MessageItem({data, currentUserId, selectedUser}) {
             return (
               <div className="p-1 mt-2 d-flex">
                   <MessageItemElement data={data}>
-                      <div className="d-flex flex-row justify-content-between align-items-center">
-                          <Typography variant={"body2"} sx={{color: '#555588'}}>{`${selectedUser?.firstName}`}</Typography>
-                          <div className="date-message-badge d-flex flex-row-reverse text-primary">{data?.timestamp}</div>
+                      <div className="d-flex flex-row justify-content-between align-items-center flex-wrap">
+                          <Typography variant={"body2"} sx={{color: '#555588', marginRight: 5}}>{`${selectedUser?.firstName}`}</Typography>
+                          <div className="date-message-badge d-block text-primary">{data?.timestamp}</div>
                       </div>
                       <div className="d-flex">{data?.message}</div>
                       <div className="date-message-badge d-flex flex-row justify-content-between align-items-center text-danger">
@@ -25,9 +25,9 @@ function MessageItem({data, currentUserId, selectedUser}) {
             return (
               <div className="p-1 mt-2 d-flex flex-row-reverse">
                   <MessageItemElement color={"#C2F78E"} data={data}>
-                      <div className="d-flex flex-row justify-content-between align-items-center">
-                          <Typography variant={"body2"} sx={{color: '#555588'}}>{MSG_YOU}</Typography>
-                          <div className="date-message-badge d-flex flex-row-reverse text-primary">{data?.timestamp}</div>
+                      <div className="d-flex flex-row justify-content-between align-items-center flex-wrap">
+                          <Typography variant={"body2"} sx={{color: '#555588', marginRight: 5}}>{MSG_YOU}</Typography>
+                          <div className="date-message-badge d-block text-primary">{data?.timestamp}</div>
                       </div>
                       <div className="d-flex">{data?.message}</div>
                       <MessageStatus isRead={data?.read}/>
